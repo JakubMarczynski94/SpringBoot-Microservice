9dee6f20dd55689cd37a8aa27946281e45529935
philljohnson <philljohnson739@gmail.com>
2020-08-10 14:15:45 +0300

    fix

--- service-parent/ticket-service/src/main/java/com/celal/repository/es/TicketElasticRepository.java
+++
@@ -1,9 +0,0 @@
-package com.celal.repository.es;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.celal.model.es.TicketModel;
-
-public interface TicketElasticRepository  extends ElasticsearchRepository<TicketModel, Long>{
-
-}
